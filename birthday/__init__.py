8b353952a4bf1eef3c0cc1cc64f14121183be9f7
NoiselessVolatileLobster <92268368+NoiselessVolatileLobster@users.noreply.github.com>
2024-11-17 19:14:46 -0600

    Update __init__.py

--- birthday/__init__.py
+++ birthday/__init__.py
@@ -16,5 +16,5 @@ with open(Path(__file__).parent / "info.json", encoding="utf8") as fp:
 
 async def setup(bot: Red) -> None:
     cog = Birthday(bot)
-    await out_of_date_check("birthday", cog.__version__)
+    await out_of_date_check("anniversary", cog.__version__)
     await bot.add_cog(cog)
