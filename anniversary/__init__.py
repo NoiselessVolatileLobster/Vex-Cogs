f081247cd662be8bedc5de4be9639c3f0ebdcf28
NoiselessVolatileLobster <92268368+NoiselessVolatileLobster@users.noreply.github.com>
2024-11-17 19:28:05 -0600

    Anniversary

--- anniversary/__init__.py
+++ anniversary/__init__.py
@@ -7,7 +7,7 @@ from redbot.core import VersionInfo
 from redbot.core.bot import Red
 
 from . import vexutils
-from .birthday import Birthday
+from .anniversary import Anniversary
 from .vexutils.meta import out_of_date_check
 
 with open(Path(__file__).parent / "info.json", encoding="utf8") as fp:
--- anniversary/anniversary.py
+++ anniversary/anniversary.py
@@ -16,7 +16,7 @@ from .vexutils.loop import VexLoop
 log = get_vex_logger(__name__)
 
 
-class Birthday(
+class Anniversary(
     commands.Cog,
     BirthdayLoop,
     BirthdayCommands,
